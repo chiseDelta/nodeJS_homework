898af634b176b26cb9b7a7251147fa903337bd06
chiseDelta <ghostymyname@gmail.com>
2023-03-08 14:53:21 +0200

    lesson3-homework? is done! +Use it for create new Project's!

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,61 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import mongoose from "mongoose";
+
+import { User } from "./models/User.model";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.get("/users", async (req: Request, res: Response) => {
+  const users = await User.find();
+
+  res.json(users);
+});
+
+app.get("/users/:userId", async (req: Request, res: Response) => {
+  const { userId } = req.params;
+  const user = await User.findById(userId);
+
+  res.json(user);
+});
+
+app.post("/users", async (req: Request, res: Response) => {
+  const body = req.body;
+  const user = await User.create(body);
+
+  res.status(201).json({
+    message: "User created!",
+    data: user,
+  });
+});
+
+app.put("/users", async (req: Request, res: Response) => {
+  const { userId } = req.params;
+  const user = req.body;
+
+  const updatedUser = await User.updateOne({ _id: userId }, user);
+
+  res.status(200).json({
+    message: "User updated!",
+    data: updatedUser,
+  });
+});
+
+app.delete("/users", async (req: Request, res: Response) => {
+  const { userId } = req.params;
+
+  await User.deleteOne({ _id: userId });
+
+  res.status(200).json({
+    message: "User deleted!",
+  });
+});
+
+const PORT = 5100;
+
+app.listen(PORT, async () => {
+  await mongoose.connect("mongodb://127.0.0.1:27017/my-server");
+  console.log(`Server has started on PORT ${PORT}`);
+});
